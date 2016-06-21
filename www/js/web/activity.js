5b5eb01932b6a2b493695cdfcb442e6652ff7839
forsrc <forsrc@163.com>
2016-06-22 00:06:06 +0900

    setup

--- www/js/web/activity.js
+++ www/js/web/activity.js
@@ -23,92 +23,57 @@ $(document).ready(function() {
     $("#logout").click(function() {
 
         sessionStorage.clear();
-        window.location.href = "index.html";
+        window.location.href = "../web/index.html";
     });
 
 
     $(".reload").attr("href", window.location.href);
     $.ajax({
         type: 'GET',
-        url: 'http://crm.primehomes.com/index.php?entryPoint=crmAppJson&session_id=' +
-            sessionStorage.sessionId + '&user_name=' +
+        url: MY_WEB_URL.listView + '?id=' + sessionStorage.sessionId + '&user_name=' +
             sessionStorage.username,
         dataType: 'json',
         beforeSend: function() {
             $(".loader").fadeIn("fast");
         },
         success: function(response) {
-            var response = $.parseJSON(JSON.stringify(
-                response));
-            console.log(JSON.stringify(response));
+            console.log(response);
+            //var response = $.parseJSON(JSON.stringify(response));
+            //console.log(JSON.stringify(response));
             $(".loader").fadeOut("slow", function() {
                 var nodeA = '';
                 var nodeB = '';
-                $.each(response, function(key,
-                    value) {
+                $.each(response, function(key, value) {
 
-                    if (value.phone ===
-                        null) {
-                        var newPhone =
-                            '<a href="javascript:void(0)"></a>';
+                    if (value.phone === null) {
+                        var newPhone = '<a href="javascript:void(0)"></a>';
                     } else {
-                        var newPhone =
-                            '<a href="tel:' +
-                            value.phone +
-                            '"><i class="material-icons">call</i></a>';
+                        var newPhone = '<a href="tel:'
+                            + value.phone + '"><i class="material-icons">call</i></a>';
                     }
-                    if (value.lead_type ==
-                        'New') {
-                        nodeA = nodeA +
-                            '<tr id="' +
-                            value.id +
-                            '">\
-                                <td class="leadonClick">' +
-                            newPhone +
-                            '</td>\
-                                <td class="leadnoClick">' +
-                            value.name +
-                            '</td>\
-                                <td class="leadnoClick">' +
-                            value.status +
-                            '</td>\
-                                <td class="leadnoClick">' +
-                            value.project_name +
-                            '</td>\
-                              </tr>';
+                    console.log(value.status);
+                    if (value.status === 'New') {
 
+                        nodeA = nodeA + '<tr id="' + value.id +'">\<td class="leadonClick">'
+                            + newPhone + '</td>\<td class="leadnoClick">'
+                            + value.name + '</td>\<td class="leadnoClick">'
+                            + value.status + '</td>\<td class="leadnoClick">'
+                            +　value.project_name +'</td>\</tr>';
 
-                    } else if (value.lead_type ==
-                        'Follow Up') {
-                        nodeB = nodeB +
-                            '<tr id="' +
-                            value.id +
-                            '">\
-                                <td class="leadonClick">' +
-                            newPhone +
-                            '</td>\
-                                <td class="leadnoClick">' +
-                            value.name +
-                            '</td>\
-                                <td class="leadnoClick">' +
-                            value.status +
-                            '</td>\
-                                <td class="leadnoClick">' +
-                            value.project_name +
-                            '</td>\
-                              </tr>';
+                    } else if (value.status === 'Follow Up') {
+                        nodeB = nodeB + '<tr id="' + value.id + '">\<td class="leadonClick">'
+                            +　newPhone + '</td>\<td class="leadnoClick">'
+                            +　value.name +　'</td>\<td class="leadnoClick">'
+                            +　value.status +　'</td>\<td class="leadnoClick">'
+                            +　value.project_name +　'</td>\</tr>';
 
                     }
                     //console.log(node);
                 });
-                $(
-                    "#activityContent #newLeads table tbody"
-                ).append(
-                    nodeA);
-                $(
-                    "#activityContent #followUpLeads table tbody"
-                ).append(
-                    nodeB);
+                $("#activityContent #newLeads table tbody").append(nodeA);
+
+                $("#activityContent #followUpLeads table tbody").append(nodeB);
+
                 if (sessionStorage.activeTab) {
                     //alert(sessionStorage.activeTab);
                     $('.activityTabs a[href="#' +
@@ -121,14 +86,8 @@ $(document).ready(function() {
                         function(e) {
                             $("html, body").stop()
                                 .animate({
-                                        scrollTop: $(
-                                                'tr#' +
-                                                sessionStorage
-                                                .leadId
-                                            )
-                                            .offset()
-                                            .top -
-                                            220
+                                        scrollTop: $('tr#' + sessionStorage.leadId)
+                                            .offset().top - 220
                                     }, 400,
                                     function() {}
                                 );
@@ -151,8 +110,7 @@ $(document).ready(function() {
             //alert(id);
             if (id != null) {
                 window.location.href = "leadDetail.html?id=" + id;
-                var activeTab = $(this).closest('.tab-pane').attr(
-                    "id");
+                var activeTab = $(this).closest('.tab-pane').attr("id");
                 sessionStorage.activeTab = activeTab;
                 sessionStorage.leadId = id;
             }
--- www/js/web/config.js
+++ www/js/web/config.js
@@ -1,8 +1,9 @@
 
 var MY_WEB_URL = {
     "login" : "http://192.168.11.8:3000/json/login.json",
+    "listView" : "http://192.168.11.8:3000/json/appListView.json",
     "index" : "../web/index.html",
     "activity" : "../web/activity.html",
     "secarch" : "../web/activity.html",
-    "detail" : "../web/loadDetail.html",
+    "detail" : "../web/loadDetail.html"
 };

ec2c758b6e8e763c18fbee6948dd44d27863faa3
forsrc <forsrc@163.com>
2016-06-21 21:17:43 +0900

    setup

---
+++ www/js/web/index.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+    if (sessionStorage.sessionId) {
+        window.location.href="//web/activity.html";
+    }
+    else
+    {
+    app.initialize();
+    $.material.init();
+    $.material.ripples();
+    sessionStorage.clear();
+    $('input').keypress(function(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if ( (code==13) || (code==10))
+        {
+            $(".login").click();
+        }
+    });
+    $(".login").click(function() {
+        var username = $("#username").val();
+        var password = $("#password").val();
+        if (username != '' && password != '') {
+            var formData = "user_name=" + username + "&password=" + password + "&login_request=1";
+            $.ajax({
+                type: 'POST',
+                url: 'http://crm.primehomes.com/index.php?entryPoint=crmAppLogin',
+                ContentType: 'multipart/form-data',
+                data: formData,
+                beforeSend: function() {
+
+                },
+                success: function(response) {
+                    var response = JSON.parse(response);
+                    if (response.id != 0) {
+                        sessionStorage.sessionId = response.id;
+                        sessionStorage.username= username;
+                        console.log(sessionStorage.sessionId);
+                        window.location.href = "/web/activity.html";
+                    } else {
+                        $("span.error").fadeIn();
+                    }
+                    window.location.href = "/web/activity.html";
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log(textStatus, errorThrown);
+                    $("#fail").snackbar("show");
+                }
+            });
+
+            //window.location.href="/web/activity.html";
+        } else {
+            $("span.error").fadeIn();
+        }
+    });
+    var lastScrollTop = 0;
+    $(window).scroll(function(event){
+       var st = $(this).scrollTop();
+       if (st > lastScrollTop){
+           //alert('downscroll code');
+       } else {
+          //alert('upscroll code');
+       }
+       lastScrollTop = st;
+    });
+}
+});
